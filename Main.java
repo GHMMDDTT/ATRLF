2cd2a43cc0294408f27595f0719b2ad43e5f3e49
mddt-account <mddt.systembot@gmail.com>
2025-10-19 00:27:38 -0600

    .

--- Main.java
+++ Main.java
@@ -1,46 +1,75 @@
-import another.tool.recognition.language.regexes.Regex;
-import another.tool.recognition.language.regexes.RexRegex;
-import another.tool.recognition.language.regexes.TokenRegex;
+import another.tool.recognition.language.Results.Result;
+import another.tool.recognition.language.rules.Rules;
+import another.tool.recognition.language.rules.TokenRules;
 import another.tool.recognition.language.tokenizer.LanguageTokenizer;
-import another.tool.recognition.language.tokenizer.RegexLanguageTokenizer;
+import another.tool.recognition.language.tokenizer.RuleLanguageTokenizer;
 import another.tool.recognition.language.tokenizer.Token;
 
 import java.util.ArrayList;
+import java.io.File;
 
 void main() {
 	long start = System.nanoTime();
-	LanguageTokenizer language = new LanguageTokenizer("""
-s"Hello"; """/*.repeat(6513)*/, new RegexLanguageTokenizer() {
+	LanguageTokenizer language = new LanguageTokenizer("154.7d".repeat(174_762), new RuleLanguageTokenizer() {
 		@Override
-		public Regex onRegex() {
-			return Alternatives(
-					identifier(),
-					string(),
-					character(),
-					symbols()
+		public Rules onRegex() {
+			return FirstOf(
+					number()
 			);
 		}
 
-		private Regex string() {
-			return Sequence(
-					Character('"'),
-					OneOrMore(Not(Character('"'))),
-					Character('"')
+		private Rules string() {
+			return Token(
+					Sequence(
+							Character('"'),
+							OptionalOrMore(Not(Character('"'))),
+							Character('"')
+					), MyTokens.StringLiteralToken
 			);
 		}
 
-		private Regex character() {
+		private Rules string_interpolation() {
+			return Token(
+					Sequence(
+							Character('$'), Character('"'),
+							OptionalOrMore(
+									FirstOf(
+											interpolation(),
+											NoneOf('"')
+									)
+							),
+							Character('"')
+					), MyTokens.StringLiteralToken
+			);
+		}
+
+		private Rules interpolation() {
+			return Token(
+					Sequence(
+							Character('$'), Character('{'),
+							FirstOf(
+									symbols(),
+									string(),
+									character(),
+									identifier()
+							),
+							Character('}')
+					), MyTokens.InterpolationLiteralToken
+			);
+		}
+
+		private Rules character() {
 			return Token(
 					Sequence(
 							Character('\''),
 							Not(Character('\'')),
 							Character('\'')
-					), MyTokens.CharacterLiteralToken
+					),MyTokens.CharacterLiteralToken
 			);
 		}
 
-		private Regex symbols() {
-			return Alternatives(
+		private Rules symbols() {
+			return FirstOf(
 					// Symbols Delimiter Separator Operators
 					Token(Character('('), MyTokens.ParenthesisLeftSymbolDelimiterSeparatorOperatorToken),
 					Token(Character(')'), MyTokens.ParenthesisRightSymbolDelimiterSeparatorOperatorToken),
@@ -79,128 +108,100 @@ s"Hello"; """/*.repeat(6513)*/, new RegexLanguageTokenizer() {
 			);
 		}
 
-		private Regex identifier() {
+		private Rules number() {
 			return Token(
-					Rex(
+					Group(
 							Sequence(
-									Alternatives(
+									CharacterRange('1', '9'),
+									OptionalOrMore(
+											CharacterRange('0', '9')
+									), Optional(
+											FirstOf(
+													Sequence(
+															Character('.'),
+															OneOrMore(
+																	CharacterRange('0', '9')
+															), Optional(
+																	Skip(
+																			FirstOf(
+																					Character('F'), Character('f'),
+																					Character('D'), Character('d')
+																			)
+																	)
+															)
+													),
+													Skip(
+															FirstOf(
+																	Character('F'), Character('f'),
+																	Character('D'), Character('d'),
+																	Character('L'), Character('l')
+															)
+													)
+											)
+									)
+							)
+					), new TokenRules.OnActionTokenSyntaxListener() {
+						@Override
+						public Enum<?>[] onActionTokenSyntax(Result<?> result, String text) {
+							if (text.contains(".")) {
+								if (text.endsWith("f") || text.endsWith("F")) return new Enum[] { MyTokens.FloatLiteralToken };
+								else if (text.endsWith("d") || text.endsWith("D")) return new Enum[] { MyTokens.DoubleLiteralToken };
+								return new Enum[] { MyTokens.DoubleLiteralToken };
+							} else if (text.endsWith("f") || text.endsWith("F")) return new Enum[] { MyTokens.FloatLiteralToken };
+							else if (text.endsWith("d") || text.endsWith("D")) return new Enum[] { MyTokens.DoubleLiteralToken };
+							else if (text.endsWith("l") || text.endsWith("L")) return new Enum[] { MyTokens.LongLiteralToken };
+							return new Enum[] { MyTokens.IntegerLiteralToken };
+						}
+					}
+			);
+		}
+
+		private Rules identifier() {
+			return Token(
+					Sequence(
+							Group(Sequence(
+									FirstOf(
+
 											CharacterRange('a', 'z'),
 											CharacterRange('A', 'Z')
-									)
-							), new RexRegex.OnRexRegexListener() {
-								@Override
-								public Regex onRexRegex(String str) {
-									if (str.equalsIgnoreCase("s")) return string();
-									return OptionalOrMore(
-											Alternatives(
+									),
+									OptionalOrMore(
+											FirstOf(
 													CharacterRange('a', 'z'),
 													CharacterRange('A', 'Z'),
 													CharacterRange('0', '9'),
 													Character('_'),
 													Character('$')
 											)
-									);
-								}
-							}
-					), new TokenRegex.OnActionTokenSyntaxListener() {
+									)
+							))
+					), new TokenRules.OnActionTokenSyntaxListener() {
 						@Override
-						public Enum<?>[] onActionTokenSyntax(String text) {
+						public Enum<?>[] onActionTokenSyntax(Result<?> result, String text) {
 							return switch (text) {
 								case "break" -> new Enum[] { MyTokens.BreakKeywordControlToken };
 								case "continue" -> new Enum[] { MyTokens.ContinueKeywordControlToken };
-								case "module" -> new Enum[] { MyTokens.ModuleKeywordToken };
+
 								case "package" -> new Enum[] { MyTokens.PackageKeywordToken };
+
 								case "import" -> new Enum[] { MyTokens.ImportKeywordToken };
 								case "from" -> new Enum[] { MyTokens.FromKeywordToken };
-								case "using" -> new Enum[] { MyTokens.UsingKeywordToken };
-								case "as" -> new Enum[] { MyTokens.AsKeywordToken };
+
 								case "protected" -> new Enum[] { MyTokens.ProtectedKeywordToken };
 								case "public" -> new Enum[] { MyTokens.PublicKeywordToken };
 								case "private" -> new Enum[] { MyTokens.PrivateKeywordToken };
-								case "switch" -> new Enum[] { MyTokens.SwitchKeywordToken };
-								case "case" -> new Enum[] { MyTokens.CaseKeywordToken };
-								case "sealed" -> new Enum[] { MyTokens.SealedKeywordToken };
-								case "external" -> new Enum[] { MyTokens.ExternalKeywordToken };
-								case "internal" -> new Enum[] { MyTokens.InternalKeywordToken };
-								case "inner" -> new Enum[] { MyTokens.InnerKeywordToken };
-								case "exner" -> new Enum[] { MyTokens.ExnerKeywordToken };
-								case "open" -> new Enum[] { MyTokens.OpenKeywordToken };
-								case "close" -> new Enum[] { MyTokens.CloseKeywordToken };
-								case "friend" -> new Enum[] { MyTokens.FriendKeywordToken };
-								case "companion" -> new Enum[] { MyTokens.CompanionKeywordToken };
-								case "final" -> new Enum[] { MyTokens.FinalKeywordToken };
-								case "abstract" -> new Enum[] { MyTokens.AbstractKeywordToken };
-								case "class" -> new Enum[] { MyTokens.ClassKeywordToken };
-								case "enumerate" -> new Enum[] { MyTokens.EnumerateKeywordToken };
-								case "structure" -> new Enum[] { MyTokens.StructureKeywordToken };
-								case "record" -> new Enum[] { MyTokens.RecordKeywordToken };
-								case "data" -> new Enum[] { MyTokens.DataKeywordToken };
-								case "interface" -> new Enum[] { MyTokens.InterfaceKeywordToken };
-								case "annotation" -> new Enum[] { MyTokens.AnnotationKeywordToken };
-								case "attribute" -> new Enum[] { MyTokens.AttributeKeywordToken };
-								case "trait" -> new Enum[] { MyTokens.TraitKeywordToken };
-								case "extend" -> new Enum[] { MyTokens.ExtendKeywordToken };
-								case "extends" -> new Enum[] { MyTokens.ExtendsKeywordToken };
-								case "implement" -> new Enum[] { MyTokens.ImplementKeywordToken };
-								case "implements" -> new Enum[] { MyTokens.ImplementsKeywordToken };
-								case "companions" -> new Enum[] { MyTokens.CompanionsKeywordToken };
-								case "friends" -> new Enum[] { MyTokens.FriendsKeywordToken };
-								case "restriction" -> new Enum[] { MyTokens.RestrictionKeywordToken };
-								case "restrictions" -> new Enum[] { MyTokens.RestrictionsKeywordToken };
-								case "permit" -> new Enum[] { MyTokens.PermitKeywordToken };
-								case "permits" -> new Enum[] { MyTokens.PermitsKeywordToken };
-								case "with" -> new Enum[] { MyTokens.WithKeywordToken };
-								case "withs" -> new Enum[] { MyTokens.WithsKeywordToken };
-								case "set" -> new Enum[] { MyTokens.SetKeywordToken };
-								case "get" -> new Enum[] { MyTokens.GetKeywordToken };
-								case "local" -> new Enum[] { MyTokens.LocalKeywordToken };
-								case "global" -> new Enum[] { MyTokens.GlobalKeywordToken };
-								case "readonly" -> new Enum[] { MyTokens.ReadonlyKeywordToken };
-								case "constant" -> new Enum[] { MyTokens.ConstantKeywordToken };
-								case "writeonly" -> new Enum[] { MyTokens.WriteonlyKeywordToken };
-								case "native" -> new Enum[] { MyTokens.NativeKeywordToken };
-								case "overwrite" -> new Enum[] { MyTokens.OverwriteKeywordToken };
-								case "write" -> new Enum[] { MyTokens.WriteKeywordToken };
+
 								case "function" -> new Enum[] { MyTokens.FunctionKeywordToken };
-								case "definition" -> new Enum[] { MyTokens.DefinitionKeywordToken };
-								case "operator" -> new Enum[] { MyTokens.OperatorKeywordToken };
-								case "prefix" -> new Enum[] { MyTokens.PrefixKeywordToken };
-								case "suffix" -> new Enum[] { MyTokens.SuffixKeywordToken };
-								case "control" -> new Enum[] { MyTokens.ControlKeywordToken };
-								case "infix" -> new Enum[] { MyTokens.InfixKeywordToken };
-								case "constructor" -> new Enum[] { MyTokens.ConstructorKeywordToken };
-								case "throw" -> new Enum[] { MyTokens.ThrowKeywordToken };
-								case "throws" -> new Enum[] { MyTokens.ThrowsKeywordToken };
-								case "namespace" -> new Enum[] { MyTokens.NamespaceKeywordToken };
-								case "in" -> new Enum[] { MyTokens.InKeywordToken };
-								case "out" -> new Enum[] { MyTokens.OutKeywordToken };
-								case "so" -> new Enum[] { MyTokens.SoKeywordToken };
-								case "of" -> new Enum[] { MyTokens.OfKeywordToken };
-								case "to" -> new Enum[] { MyTokens.ToKeywordToken };
-								case "let" -> new Enum[] { MyTokens.LetKeywordToken };
-								case "type" -> new Enum[] { MyTokens.TypeKeywordToken };
+
 								case "super" -> new Enum[] { MyTokens.SuperKeywordToken };
-								case "base" -> new Enum[] { MyTokens.BaseKeywordToken };
 								case "this" -> new Enum[] { MyTokens.ThisKeywordToken };
-								case "self" -> new Enum[] { MyTokens.SelfKeywordToken };
-								case "next" -> new Enum[] { MyTokens.NextKeywordToken };
-								case "previous" -> new Enum[] { MyTokens.PreviousKeywordToken };
+
 								case "return" -> new Enum[] { MyTokens.ReturnKeywordToken };
 								case "new" -> new Enum[] { MyTokens.NewKeywordToken };
 								case "false" -> new Enum[] { MyTokens.FalseKeywordLiteralToken };
 								case "true" -> new Enum[] { MyTokens.TrueKeywordLiteralToken };
-								case "indeterminate" -> new Enum[] { MyTokens.IndeterminateKeywordLiteralToken };
-								case "undefined" -> new Enum[] { MyTokens.UndefinedKeywordLiteralToken };
-								case "NaN" -> new Enum[] { MyTokens.NaNKeywordLiteralToken };
-								case "NA" -> new Enum[] { MyTokens.NAKeywordLiteralToken };
 								case "null" -> new Enum[] { MyTokens.NullKeywordLiteralToken };
-								case "none" -> new Enum[] { MyTokens.NoneKeywordLiteralToken };
-								case "infinity" -> new Enum[] { MyTokens.InfinityKeywordLiteralToken };
-								case "neutral" -> new Enum[] { MyTokens.NeutralKeywordLiteralToken };
-								default -> {
-									if (text.startsWith("s\"") && text.endsWith("\"")) yield new Enum[] { MyTokens.StringLiteralToken };
-									yield  new Enum[] { MyTokens.IdentifierToken };
-								}
+								default -> new Enum[] { MyTokens.IdentifierToken };
 							};
 						}
 					}
@@ -208,12 +209,15 @@ s"Hello"; """/*.repeat(6513)*/, new RegexLanguageTokenizer() {
 		}
 
 		@Override
-		public Regex onRegexSkipped() {
-			return OneOrMore(Alternatives(Character(' '), Character('\r'), Character('\n'), Character('\t')));
+		public Rules onRegexSkipped() {
+			return OptionalOrMore(FirstOf(Character(' '), Character('\r'), Character('\n'), Character('\t')));
 		}
 	});
 	ArrayList<Token> tokens = language.onTokenizer();
 	long end = System.nanoTime();
-	System.out.println("Time of Execute this is in: " + ((end - start) / 1_000_000) + "ms");
-	System.out.println("Tokens: " + tokens);
+	System.out.println("Time of Execute this is in: " + ((end - start) / 1_000_000) + "ms"); // Time of Execute this is in: 29ms
+	//System.out.println("Tokens: " + tokens); // Tokens: [Token{value='[i, Token{value='[m, p, o, r, t]', type=[IdentifierToken], line=1}]', type=[ImportKeywordToken], line=1}]
 }
+
+/*
+ */
--- another/tool/recognition/language/tokenizer/LanguageTokenizer.java
+++ another/tool/recognition/language/tokenizer/LanguageTokenizer.java
@@ -1,43 +1,54 @@
 package another.tool.recognition.language.tokenizer;
 
+import another.tool.recognition.language.Results.Result;
 import another.tool.recognition.language.Results.State;
-import another.tool.recognition.language.regexes.Regex;
+import another.tool.recognition.language.rules.Rules;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 
 public class LanguageTokenizer {
 	private final LanguageTokenizerInformation information;
-	private final RegexLanguageTokenizer syntax;
+	private final RuleLanguageTokenizer syntax;
+	private final ArrayList<Token> tokens = new ArrayList<>(32);
 
-	public LanguageTokenizer(char[] text, RegexLanguageTokenizer syntax) {
+	public LanguageTokenizer(char[] text, RuleLanguageTokenizer syntax) {
 		this.information = new LanguageTokenizerInformation(text);
 		this.syntax = syntax;
 	}
 
-	public LanguageTokenizer(String text, RegexLanguageTokenizer syntax) {
+	public LanguageTokenizer(String text, RuleLanguageTokenizer syntax) {
+		this.information = new LanguageTokenizerInformation(text);
+		this.syntax = syntax;
+	}
+
+	public LanguageTokenizer(File text, RuleLanguageTokenizer syntax) {
 		this.information = new LanguageTokenizerInformation(text);
 		this.syntax = syntax;
 	}
 
 	public ArrayList<Token> onTokenizer() {
-		State state;
-		Regex syntaxesSkipped = this.syntax.onRegexSkipped();
-		Regex syntaxes = this.syntax.onRegex();
+		Result<?> state;
+		Rules syntaxesSkipped = this.syntax.onRegexSkipped();
+		Rules syntaxes = this.syntax.onRegex();
 
-		while (this.information.getCursor() < this.information.getTarget().length) {
-			state = syntaxesSkipped.onRegexExecute(this.information);
-
-			int oldCursor = this.information.getCursor();
-			if (state == State.FAILED) {
-				this.information.setCursor(oldCursor);
-			}
+		while (this.information.getPosition() < this.information.getTarget().length) {
+			syntaxesSkipped.onRegexExecute(this.information);
 
 			state = syntaxes.onRegexExecute(this.information);
 
-			if (state == State.FAILED) {
-				throw new RuntimeException("Contains error the syntax: " + syntaxes + " in " + this.information.getLine() + ':' + (this.information.getCursor() - this.information.getColumn()) + " of the input: '" + this.information.getTarget()[this.information.getCursor()] + '\'');
+			if (state.isFailed()) {
+				throw new RuntimeException("Contains error the syntax: " + syntaxes + " in " + this.information.getLine() + ':' + (this.information.getPosition() - this.information.getColumn()) + " of the input: '" + this.information.getTarget()[this.information.getPosition() - 1] + '\'');
+			}
+
+			if (state.getValue() instanceof Token) this.tokens.add((Token) state.getValue());
+			else if (state.getValue() instanceof Result<?> result) {
+				if (result.getValue() instanceof Token) this.tokens.add((Token) result.getValue());
+				else if (result.getValue() instanceof ArrayList<?>) this.tokens.addAll((Collection<? extends Token>) result);
 			}
 		}
-		return this.information.getTokens();
+		return this.tokens;
 	}
 }
